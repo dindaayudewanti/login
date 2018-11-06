bca62130855ab61a4bccdb5a77fb20278b72d56f
a <dinda.ayudewanti.da@gmail.com>
2018-11-06 21:31:18 +0700

    form login

---
+++ app/src/main/java/com/learning/login/MainActivity.java
@@ -0,0 +1,110 @@
+package com.learning.login;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+
+    Button b1, b2;
+
+    EditText ed1, ed2;
+
+    TextView tx1;
+
+
+    int counter = 3;
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+
+        b1 = findViewById(R.id.btnLogin);
+
+        b2 = findViewById(R.id.btnCancel);
+
+        ed1 = findViewById(R.id.editUser);
+
+        ed2 = findViewById(R.id.editPass);
+
+
+        tx1 = findViewById(R.id.textView2);
+
+        tx1.setVisibility(View.GONE);
+
+
+        b1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View v) {
+
+
+                //set username dan password dengan "admin"
+
+                if (ed1.getText().toString().equals("admin") &&
+
+                        ed2.getText().toString().equals("admin"))
+
+
+                    //kondisi jika login benar
+
+                    Toast.makeText(getApplicationContext(), "Login Sukses", Toast.LENGTH_SHORT).show();
+
+                else {
+
+                    //jika login gagal
+
+
+                    Toast.makeText(getApplicationContext(), "Username atau Password Anda Salah",
+
+                            Toast.LENGTH_SHORT).show();
+
+
+                    tx1.setVisibility(View.VISIBLE);
+
+                    tx1.setBackgroundColor(Color.RED);
+
+                    counter--;
+
+                    tx1.setText(Integer.toString(counter));
+
+
+                    if (counter == 0) {
+
+                        b1.setEnabled(false);
+
+                    }
+
+                }
+
+
+            }
+
+        });
+
+
+        b2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View v) {
+
+                finish();
+
+            }
+
+        });
+
+    }
+
+}
